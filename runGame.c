aae0af05a006f356bc828c75e50ab69c5e483c2c
ThomasWaring <ThomasWaring@users.noreply.github.com>
2014-05-07 15:39:05 +0800

    Updated runGame.c

--- runGame.c
+++ runGame.c
@@ -5,19 +5,26 @@
 
 #define WIN_KPI 150
 
+#define DICE_LOWERBOUND_START_SPINOFF 0
+#define DICE_UPPERBOUND_START_SPINOFF 2
+#define DICE_VAL_FOR_IP 0
+
 int main (int argc, char *argv[]) {
   Game g  = newGame(..);
   int currentPlayer = 0;
   
-  While (getKPI (g, currentPlayer) < WIN_KPI) {
+  while (getKPI (g, currentPlayer) < WIN_KPI) {
     diceValue = rollDice();
     throwDice(g, diceValue);
     action move = getMove();
-    While (move.actionCode != PASS) {
-      assert (isLegal (g, move) == TRUE);
-      If(move.actionCode == START_SPINOFF) {
-        diceValue = rollDice();
-        Decide
+    while (move.actionCode != PASS) {
+      assert (isLegalAction (g, move) == TRUE);
+      if (move.actionCode == START_SPINOFF) {
+        diceValue = rollDice(DICE_LOWERBOUND_START_SPINOFF,
+                              DICE_UPPERBOUND_START_SPINOFF);
+        if (diceValue == DICE_VAL_FOR_IP) {
+          
+        }
       }
     makeAction(g, move);
     }
@@ -34,12 +41,29 @@ action getMove () {
   
   // Get extra information if necessary
   if (move.actionCode == 1) {
-    scanf ("Enter the destination path for the campus: %s", &move.destination);
+    printf ("Enter the destination path for the campus: ");
+    scanf ("%s", move.destination);
   } else if (move.actionCode == 2) {
-    scanf ("Enter the destination path for the GO8 campus: %s", &move.destination);
+    printf ("Enter the destination path for the GO8 campus: ");
+    scanf ("%s", move.destination);
   } else if (move.actionCode == 3) {
-    scanf ("Enter the destination path for the ARC: %s", &move.destination);
+    printf ("Enter the destination path for the ARC: ");
+    scanf ("%s", move.destination);
   }else if (move.actionCode == 7) {
-    scanf ("Enter the student type to retrain: %d", &move.disciplineFrom);
+    printf ("Enter the student type to retrain: ");
+    scanf ("%d", move.disciplineFrom);
+    printf ("Enter the student type to retrain to: ");
+    scanf ("%d", &move.desciplineTo);
   }
 }
+
+// Get a user inputted dice value
+// Limits are inclusive
+int getDiceValueTwixt (int lowerbound, int upperbound) {
+  int val;
+  printf ("Enter a value between %d and %d", lowerbound, upperbound);
+  scanf ("%d", &val);
+}
+
+
+
